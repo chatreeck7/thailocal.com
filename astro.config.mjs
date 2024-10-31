ccd4db0d6a83e0890176e93434b9301ae1736c47
chatreeck7 <chatree.devl@gmail.com>
2024-10-31 17:34:31 +0700

    Setup: set the shadcn/ui and React to Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import robotsTxt from 'astro-robots-txt';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://promptpage.org/',
-  integrations: [robotsTxt(), sitemap(), tailwind()]
+  integrations: [robotsTxt(), sitemap(), tailwind({
+    applyBaseStyles: false
+  }), react()]
 });
